77b7363560bfc2fd8b9d317f9ac8dc87fbb2882a
vaishud12 <vaishuu2397@gmail.com>
2021-07-13 22:12:34 +0530

    card setup

---
+++ index.js
@@ -0,0 +1,7 @@
+const saveChanges = () => {
+    const taskData = {
+        id: '${Data.now()}', //unique number for id
+        imageUrl: document.getElementById,("imageurl"),
+        tasktitle: document.getElementById,("")
+    }
+}
